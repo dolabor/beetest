2024490bf84ff64c947e32520f79deeed0284c25
Евгения Чигжит <evgenia@MacBook-Air-Evgenia.local>
2022-02-17 23:42:44 +0300

    AppHeader has been added

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.css';
+
+function AppHeader() {
+    return <div className='AppHeader'>
+        <span> FAQ </span>
+        <span> Поддержка </span>
+        <span> Настройки </span>
+    </div>
+}
+
+export default AppHeader;
